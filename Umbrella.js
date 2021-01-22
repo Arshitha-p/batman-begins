bb9c4e35fb6a4937362474441cfaac97ffde3ddc
Arshitha <72261851+Arshitha-p@users.noreply.github.com>
2021-01-22 19:08:20 +0530

    Add files via upload

--- Umbrella.js
+++ Umbrella.js
@@ -1,17 +1,19 @@
 class Umbrella {
-    constructor(x,y){
-        var options = {
-            isStatic: true,
-        }
-        this.image = loadImage("Walking Frame/walking_1.png");
-        this.umbrella = Bodies.circle(x,y,50,options);
-        this.radius = 50;
-        World.add(world, this.umbrella)
-    }
+      
+     constructor(){
+          var options = {
+               isStatic: true,
+               restitution: 0.5
+          };
+          var radius = 25;
+          this.body = Matter.Bodies.circle(100,350, radius, options);
+          this.width = radius;
+          this.height = radius;
+          World.add(world, this.body);
+     }
+     display(){
+          var pos = this.body.position;
+          image(man_image, pos.x-90, pos.y-20,175,175);
 
-    display(){
-        var pos = this.umbrella.position;
-        imageMode(CENTER);
-        image(this.image,pos.x,pos.y+70,300,300);
-    }
+     }
 }
